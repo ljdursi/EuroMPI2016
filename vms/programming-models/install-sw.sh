3c3af5d5f08599eb833d4bf33a4190aa88c89b74
Jonathan Dursi <jonathan@dursi.ca>
2016-09-11 12:01:27 -0400

    Get rid of Hadoop in install, and hadoop configs

--- vms/programming-models/install-sw.sh
+++ vms/programming-models/install-sw.sh
@@ -4,7 +4,10 @@ sudo apt-get install -y git
 sudo apt-get install -y make
 sudo apt-get install -y openjdk-7-jre-headless 
 sudo apt-get install -y scala
+sudo apt-get install -y python-matplotlib
 sudo apt-get install -y vim nano emacs  # yes, even emacs
+
+sudo ln -s /usr/lib/jvm/java-7-openjdk-amd64/ /usr/lib/jvm/default-java
 
 readonly BASEDIR=~vagrant
 readonly BASHRC=${BASEDIR}/.bashrc
@@ -40,49 +43,43 @@ echo "Dask install"
 /miniconda/bin/pip install dask[complete]
 /miniconda/bin/pip install graphviz
 
-# Hadoop
-echo "Hodoop install"
-APACHE_MIRROR=mirror.csclub.uwaterloo.ca/apache/
-HADOOP_VERSION=2.6.4
-if [ ! -d hadoop-${HADOOP_VERSION} ]
-then
-  HADOOP_TAR_GZ=hadoop-$HADOOP_VERSION.tar.gz
-  wget -q --tries=1 --timeout=15 http://${APACHE_MIRROR}/hadoop/common/hadoop-${HADOOP_VERSION}/$HADOOP_TAR_GZ
-  tar xzf $HADOOP_TAR_GZ
-  chown -R "${USER}.${GROUP}" "hadoop-${HADOOP_VERSION}"
-  rm "$HADOOP_TAR_GZ"
-fi
+readonly APACHE_MIRROR=mirror.csclub.uwaterloo.ca/apache/
 
 # Spark
 echo "Spark install"
 cd "${BASEDIR}"
-SPARK_VERSION=1.6.2   # Toree doesn't yet support 2.0.0
-SPARK_TAR_GZ=spark-${SPARK_VERSION}-bin-hadoop2.6.tgz   
-SPARK_DIR=$( basename ${SPARK_TAR_GZ} .tgz )
+readonly SPARK_VERSION=1.6.2   # Toree doesn't yet support 2.0.0
+readonly SPARK_TAR_GZ=spark-${SPARK_VERSION}-bin-hadoop2.6.tgz   
+readonly SPARK_DIR=$( basename ${SPARK_TAR_GZ} .tgz )
 if [ ! -d "${SPARK_DIR}" ]
 then
   wget -q --tries=1 --timeout=15 http://${APACHE_MIRROR}/spark/spark-${SPARK_VERSION}/${SPARK_TAR_GZ} -O ${SPARK_TAR_GZ}
   tar xzf ${SPARK_TAR_GZ} 2> /dev/null
   chown -R "${USER}.${GROUP}" "${SPARK_DIR}"
   rm ${SPARK_TAR_GZ}
+  export SPARK_HOME=${BASEDIR}/${SPARK_DIR}" >> "${BASHRC}
   echo "export SPARK_HOME=${BASEDIR}/${SPARK_DIR}" >> "${BASHRC}"
   py4jzip=$( find "${BASEDIR}/${SPARK_DIR}/python" -name "py4j*src.zip" )
   echo "export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/${SPARK_DIR}/python:${py4jzip}" >> "${BASHRC}"
+  cp ${SPARK_HOME}/conf/log4j.properties.template ${SPARK_HOME}/conf/log4j.properties
+  sed -i -e 's/log4j.rootCategory=INFO, console/log4j.rootCategory=WARN, console/' ${SPARK_HOME}/conf/log4j.properties
 fi
-export SPARK_HOME=${BASEDIR}/${SPARK_DIR}
 export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/${SPARK_DIR}/python
 py4jzip=$( find "${BASEDIR}/${SPARK_DIR}/python" -name "py4j*src.zip" )
 export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/${SPARK_DIR}/python:${py4jzip}
+echo "export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/${SPARK_DIR}/python" >> ~/.bashrc
+echo "export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/${SPARK_DIR}/python:${py4jzip}" >> ~/.bashrc
 
 /miniconda/bin/pip install findspark
 /miniconda/bin/pip install toree
-jupyter toree install --spark_home=${SPARK_HOME}
+jupyter toree install --spark_home=${SPARK_HOME} --interpreters=Scala,PySpark
 
 # install graphframes
-cd "${BASEDIR}"
-"$SPARK_HOME/bin/spark-shell" --packages graphframes:graphframes:0.2.0-spark1.6-s_2.10 <<EOF
+cd "${SPARK_HOME}"
+./bin/spark-shell --packages graphframes:graphframes:0.2.0-spark1.6-s_2.10 <<EOF
 EOF
-rm "${BASEDIR}/derby.log"
+rm "derby.log"
+cd ${BASEDIR}
 
 # Tensorflow
 echo "Tensorflow install"
@@ -96,9 +93,11 @@ git clone https://github.com/shellinabox/shellinabox.git && cd shellinabox && au
 
 # Chapel
 echo "Chapel install"
-CHAPEL_VERSION=1.12.0
-CHAPEL_TAR_GZ=chapel-${CHAPEL_VERSION}.tar.gz
-CHAPEL_DIR=$( basename ${CHAPEL_TAR_GZ} .tar.gz )
+# turn off memory randomization for GASNET
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+readonly CHAPEL_VERSION=1.12.0
+readonly CHAPEL_TAR_GZ=chapel-${CHAPEL_VERSION}.tar.gz
+readonly CHAPEL_DIR=$( basename ${CHAPEL_TAR_GZ} .tar.gz )
 if [ ! -d "$CHAPEL_DIR" ]
 then
     cd "${BASEDIR}"
@@ -124,25 +123,20 @@ fi
 cd ${BASEDIR}
 git clone https://github.com/ljdursi/EuroMPI2016.git
 chown -R ${USER}.${GROUP} EuroMPI2016
-for dir in bin examples hadoop-config
+for dir in examples 
 do
    cp -r EuroMPI2016/${dir} ${BASEDIR}
    chown -R ${USER}.${GROUP} ${BASEDIR}/${dir}
 done
+
 sudo mv ${BASEDIR}/EuroMPI2016/vms/programming-models/shellinabox /etc/init.d
 sudo mv ${BASEDIR}/EuroMPI2016/vms/programming-models/jupyter /etc/init.d
 chmod 755 /etc/init.d/{jupyter,shellinabox}
 sudo update-rc.d shellinabox  defaults
 sudo update-rc.d jupyter  defaults
+
 rm -rf EuroMPI2016
-chown -R ${USER}.${GROUP} ${BASEDIR}/examples
-
-echo "source ~/bin/setup.sh" >> ${BASHRC}
-
-for file in ${BASEDIR}/hadoop-config/etc/hadoop/* ; do mv $file ${BASEDIR}/hadoop-${HADOOP_VERSION}/etc/hadoop/; done
-rm -rf hadoop-config
-export HADOOP_CONF_DIR="${BASEDIR}/hadoop-${HADOOP_VERSION}"
-echo "export HADOOP_CONF_DIR=\"${BASEDIR}/hadoop-${HADOOP_VERSION}\"" >> ~/.bashrc
+chown -R ${USER}.${GROUP} ${BASEDIR}
 
 #
 # Start up services: 
